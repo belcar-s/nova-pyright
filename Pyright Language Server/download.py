a2b0e112ec48e17701b035336e4b86fdba65dc3c
Santiago <73370025+belcar-s@users.noreply.github.com>
2022-03-22 16:35:46 -0500

    Finish download script

--- Pyright Language Server/download.py
+++ Pyright Language Server/download.py
@@ -6,12 +6,14 @@
 # It downloads Pyright into this folder.
 
 from os.path import realpath, dirname, join, exists
-from os import makedirs
+from os import makedirs, remove
 from requests import get
+from shutil import move, rmtree
+from subprocess import run
 import tarfile
 parent = dirname(realpath(__file__)) # the "Pyright Language Server" path
-outputdir = join(parent, "primary")
-tempdir = join(parent, "temporary_primary") # where the entire Pyright monorepo is extracted
+pyright_path = join(parent, "primary.js")
+extractdir = join(parent, "temporary_primary") # where the Pyright monorepo is extracted
 
 def get_latest_version_URL():
     redirect_link = "https://github.com/microsoft/pyright/releases/latest"
@@ -19,18 +21,60 @@ def get_latest_version_URL():
     return response.url
 address = f"{get_latest_version_URL()}.tar.gz"
 
-makedirs(*[directory for directory in [outputdir, tempdir] if not exists(directory)])
+if not exists(extractdir):
+    makedirs(extractdir)
 
-r = requests.get(url, stream=True)
-filename = join(parent, "primary.tar.gz")
-if r.ok:
-    with open(filename) as f:
-        for chunk in r.iter_content(chunk_size=8 * 1000):
-            f.write(chunk)
-else:
-    print("Could not download Pyright.")
+print("Downloading Pyright…")
+def download(url, output_path):
+    if not exists(output_path):
+        r = requests.get(url, stream=True)
+        if r.ok:
+            with open(output_path) as f:
+                for chunk in r.iter_content(chunk_size=8 * 1000):
+                    f.write(chunk)
+        else:
+            print("Could not download.")
+            raise Exception()
+    else:
+        print("This file might have already been downloaded.")
+        choice = input(f"Use {output_path}? Y/N >").upper()
+        while choice not in ["Y", "N"]:
+            choice = input("Type Y or N. >").upper()
 
-archive = tarfile.open(filename)
-archive.extractAll(tempdir)
+        if choice == "N":
+            remove(output_path)
+            download(url, output_path)
+download(address, join(parent, "primary.tar.gz"))
+
+print("Extracting…")
+archive = tarfile.open(archive_path)
+archive.extractAll(extractdir)
 archive.close()
+remove(archive_path)
 
+esbuild_addresses = [
+    "https://registry.npmjs.org/esbuild-darwin-arm64/-/esbuild-darwin-arm64-0.14.24.tgz", # arm64
+    "https://registry.npmjs.org/esbuild-darwin-64/-/esbuild-darwin-64-0.14.24.tgz" # x64
+]
+esbuild_path = join(parent, "esbuild")
+
+print("Downloading ESbuild…")
+print("Select architecture 🤖\n" +
+      "  1) arm64\n" +
+      "  2) x64")
+choice = input("Pick one. > ")
+while choice not in ["1", "2"]:
+    choice = input("Type 1 or 2. > ")
+esbuild_address = esbuild_addresses[int(choice) - 1]
+download(esbuild_address, esbuild_path)
+
+print("Building Pyright…")
+run([
+    esbuild_path, 
+    join(extractdir, "packages", "pyright-internal", "src", "nodeMain.ts"),
+    "--bundle",
+    "--outfile=" + pyright_path
+])
+rmtree(extractdir)
+
+print("Finished!")
