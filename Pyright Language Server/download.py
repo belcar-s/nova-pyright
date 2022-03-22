d0070a7d2d8a9e85c8944d69e22612bb1e6a009c
Santiago <73370025+belcar-s@users.noreply.github.com>
2022-03-21 21:26:44 -0500

    Continue work on downloader script

--- Pyright Language Server/download.py
+++ Pyright Language Server/download.py
@@ -7,14 +7,24 @@
 
 from os.path import realpath, dirname, join, exists
 from os import makedirs
+from requests import get
 parent = dirname(realpath(__file__)) # the "Pyright Language Server" path
 dir = join(parent, "primary")
 
 def get_latest_version_URL():
     redirect_link = "https://github.com/microsoft/pyright/releases/latest"
-    
+    response = get(redirect_link)
+    return response.url
 address = f"{get_latest_version_URL()}.tar.gz"
 
 if not exists(dir):
     makedirs(dir)
 
+r = requests.get(url, stream=True)
+if r.ok:
+    filename = join(parent, "primary.tar.gz")
+    with open(filename) as f:
+        for chunk in r.iter_content(chunk_size=8 * 1000):
+            f.write(chunk)
+   
+        
