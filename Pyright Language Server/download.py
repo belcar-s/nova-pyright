a612d51376993a4ead3a928e4e9d157d25698bc9
Santiago Cuentas-Zavala <mantrap.tartan0c@icloud.com>
2022-03-25 19:53:51 -0500

    Clean up script

--- Pyright Language Server/download.py
+++ Pyright Language Server/download.py
@@ -8,27 +8,20 @@
 from os.path import realpath, dirname, join, exists
 from os import makedirs, remove
 from requests import get
-from shutil import move, rmtree
+from shutil import move, rmtree, which
 from subprocess import run
 import tarfile
-parent = dirname(realpath(__file__)) # the "Pyright Language Server" path
-pyright_path = join(parent, "primary.js")
-extractdir = join(parent, "temporary_primary") # where the Pyright monorepo is extracted
-
 def get_latest_version_number():
     redirect_link = "https://github.com/microsoft/pyright/releases/latest"
     response = get(redirect_link)
     return response.url.split("/")[-1]
-address = f"https://github.com/microsoft/pyright/archive/refs/tags/{get_latest_version_number()}.tar.gz"
-print(address)
 
-if not exists(extractdir):
-    makedirs(extractdir)
+parent = dirname(realpath(__file__)) # the "Pyright Language Server" path
 
+# =========
 print("Downloading Pyright…")
 def download(url, output_path):
     if not exists(output_path):
-        
         r = get(url, stream=True)
         if r.ok:
             with open(output_path, "wb") as f:
@@ -48,22 +41,20 @@ def download(url, output_path):
             remove(output_path)
             download(url, output_path)
 archive_path = join(parent, "primary.tar.gz")
+address = f"https://github.com/microsoft/pyright/archive/refs/tags/{get_latest_version_number()}.tar.gz"
 download(address, archive_path)
 
+# =========
 print("Extracting…")
+extractdir = join(parent, "temporary_primary") # where the Pyright monorepo is extracted
+if not exists(extractdir):
+    makedirs(extractdir)
 archive = tarfile.open(archive_path)
 archive.extractall(extractdir)
 archive.close()
 remove(archive_path)
 
-esbuild_addresses = [
-    "https://registry.npmjs.org/esbuild-darwin-arm64/-/esbuild-darwin-arm64-0.14.24.tgz", # arm64
-    "https://registry.npmjs.org/esbuild-darwin-64/-/esbuild-darwin-64-0.14.24.tgz", # x64
-  "https://registry.npmjs.org/esbuild-linux-64/-/esbuild-linux-64-0.14.24.tgz"
-]
-esbuild_path = join(parent, "esbuild")
-esbuild_archive_path = join(parent, "esbuild.tgz")
-esbuild_extraction = join(parent, "esbuild_temp")
+# =========
 print("Downloading ESbuild…")
 print("Select architecture 🤖\n" +
       "  1) arm64\n" +
@@ -71,34 +62,54 @@ print("Select architecture 🤖\n" +
 choice = input("Pick one. > ")
 while choice not in ["1", "2", "3"]:
     choice = input("Type 1 or 2. > ")
+esbuild_addresses = [
+    "https://registry.npmjs.org/esbuild-darwin-arm64/-/esbuild-darwin-arm64-0.14.24.tgz", # arm64
+    "https://registry.npmjs.org/esbuild-darwin-64/-/esbuild-darwin-64-0.14.24.tgz", # x64
+    "https://registry.npmjs.org/esbuild-linux-64/-/esbuild-linux-64-0.14.24.tgz" # secret Linux version
+]
 esbuild_address = esbuild_addresses[int(choice) - 1]
+esbuild_archive_path = join(parent, "esbuild.tgz")
 download(esbuild_address, esbuild_archive_path)
 
-if not exists(esbuild_extraction):
-  makedirs(esbuild_extraction)
+# Extraction of ESBuild
+# =========
+esbuild_extraction_path = join(parent, "esbuild_temp")
+if not exists(esbuild_extraction_path):
+    makedirs(esbuild_extraction_path)
 
 esbuild_archive = tarfile.open(esbuild_archive_path)
-esbuild_archive.extractall(esbuild_extraction)
+esbuild_archive.extractall(esbuild_extraction_path)
 esbuild_archive.close()
 
-move(join(esbuild_extraction, "package", "bin", "esbuild"), esbuild_path)
-rmtree(esbuild_extraction)
+esbuild_path = join(parent, "esbuild")
+move(join(esbuild_extraction_path, "package", "bin", "esbuild"), esbuild_path)
+rmtree(esbuild_extraction_path)
 
+# =========
 print("Building Pyright…")
+
+# ======
 print("(Installing…)")
+npm_path = which("npm")
+if not npm_path:
+    npm_path = input("Please enter NPM's path >")
+
+# Install Pyright dependencies
 run([
-  "npm",
+  npm_path,
   "install"
 ],
   cwd=join(extractdir, "pyright-" + get_latest_version_number(), "packages", "pyright-internal"))
 
+# ======
 print("(Bundling…)")
+pyright_path = join(parent, "primary.js")
 run([
     esbuild_path, 
     join(extractdir, "pyright-" + get_latest_version_number(), "packages", "pyright-internal", "src", "nodeMain.ts"),
     "--bundle",
-    "--outfile=" + pyright_path,
-    "--log-level=verbose"
+    "--platform=node",
+    "--outfile=" + pyright_path
 ])
 
 print("Finished!")
