b02ba2c3b6b8781bc083a63ec902512783cbfb62
Santiago Cuentas-Zavala <mantrap.tartan0c@icloud.com>
2022-03-31 14:38:36 -0500

    Actually enable non-interactive use by removing features :(

---
+++ Scripts/download.js
@@ -0,0 +1,21 @@
+// This function is to aid in running the Python download
+// script. It returns a Promise that is fulfilled/rejected
+// when the script finishes running.
+const { downloaderPath } = require("./paths.js");
+
+exports.downloadLanguageServer = (name) => {
+	const options = {
+		args: ["python3", downloaderPath, name]
+	};
+	const process = new Process("/usr/bin/env", options);
+
+	const onExit = new Promise((resolve, reject) => {
+		process.onDidExit(status => {
+			const action = (status == 0) ? resolve : reject;
+			action(status);
+		});
+	});
+
+	process.start();
+	return onExit;
+};
